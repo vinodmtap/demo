764ebd5e2be045bc6d35d77a87dc40a45ba6aafe
vinodmtap <vinod.k@mtap.in>
2022-04-12 16:15:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Settings from './components/Settings';
+
+function App() {
+  return (
+    <div className="App">
+      <Settings/>
+    </div>
+  );
+}
+
+export default App;
