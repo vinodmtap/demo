6a8e5c84ec5ae47bc03896fe8a79b619b56ef536
vinodmtap <vinod.k@mtap.in>
2022-04-25 09:50:01 +0530

    message in quote

--- src/components/TablePagination.js
+++ src/components/TablePagination.js
@@ -3,11 +3,16 @@ import React, { useEffect, useState } from 'react'
 
 const TablePagination = ({showPerPage, onPaginationChange, total}) => {
     const[counter, setCounter] = useState(1);
+    const[numberOfButtons, setNumberOfButtons] = useState(0);
 
     useEffect(()=>{
         const value = showPerPage*counter;
         onPaginationChange((value-showPerPage), value);
     },[counter])
+
+    useEffect(()=>{
+        setNumberOfButtons(Math.ceil(total/showPerPage));
+    },[total]);
     //  console.log(onPaginationChange);
     const onButtonClick = (type) => {
         if(type === "prev"){
@@ -27,6 +32,9 @@ const TablePagination = ({showPerPage, onPaginationChange, total}) => {
   return (
     <Box>
         <Button onClick={()=>onButtonClick("prev")}>PREV</Button>
+        {new Array(numberOfButtons).fill("").map((el,index)=>(
+            <Button m={2} onClick={()=>setCounter(index+1)} key={index}>{index+1}</Button>
+        ))}
         <Button onClick={()=>onButtonClick("next")}>NEXT</Button>
     </Box>
   )
